33cc814b11c6a25aa011cd796eec7b6cd93d858a
ilya <shakurov.ia@gmail.com>
2022-11-05 16:47:17 +0300

    feat: add solution for 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('fs/promises');
+
+const sourceFolder = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css')
+
+
+async function deleteBoundle() {
+  fs.unlink(bundle, (err) => {
+    if (err) console.error(err.message);
+  });
+}
+
+async function copyFiles() {
+  const sourceFiles = await readdir(sourceFolder, {withFileTypes: true});
+
+  for (let file of sourceFiles) {
+    try {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const sourceFile = path.join(sourceFolder, file.name);
+        const rs = fs.createReadStream(sourceFile);
+
+        rs.on('data', chunk => {
+          fs.appendFile(bundle, chunk.toString(),
+            err => {
+              if (err) throw err;
+            }
+          );
+        })
+        rs.on('end', () => console.log(`${file.name} added to boundle.css`));
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+}
+
+deleteBoundle();
+copyFiles();
